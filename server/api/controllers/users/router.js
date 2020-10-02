a243a1a5c85584ff62071d6433cd4a7301d9f8ec
Kunal Jain <kunaljain0212@pop-os.localdomain>
2020-10-02 20:44:09 +0530

    resolving

--- server/api/controllers/users/router.js
+++ server/api/controllers/users/router.js
@@ -1,12 +1,32 @@
-import * as express from "express";
-import controller from "./controller";
+import * as express from 'express';
+import controller from './controller';
+import isAuthenticated from '../../middlewares/isAuthenticated';
+const multer = require('multer');
 
-import isAuthenticated from "../../middlewares/isAuthenticated";
+const fileFilter = (req, file, callback) => {
+  const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+  if (!acceptedImageTypes.includes(file.mimetype))
+    return callback(new Error('Please upload a valid Image file'), false);
+  else return callback(null, true);
+};
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, __dirname + `/../../../../public/images/`);
+  },
+  filename: (req, file, callback) => {
+    callback(null, req.user.roll+file.originalname);
+  },
+});
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+}).single('dp');
 
 export default express
   .Router()
-  .post("/login", controller.login)
-  .put("/password", isAuthenticated, controller.changePassword)
-  .put("/dp", isAuthenticated, controller.changeProfilePicture)
-  .get("/details", isAuthenticated, controller.getUserDetails)
-  .get("/leaderboard", isAuthenticated, controller.getLeaderboard);
+  .post('/login', controller.login)
+  .put('/password', isAuthenticated, controller.changePassword)
+  .put('/dp', isAuthenticated, upload, controller.changeProfilePicture)
+  .get('/dp', isAuthenticated, controller.getProfilePicture)
+  .get('/details', isAuthenticated, controller.getUserDetails)
+  .get('/leaderboard', isAuthenticated, controller.getLeaderboard);
